b4cf2f3b87c1d5564e864aa5c0f2bb983a0648f4
Paul O <paul.orawiec@gmail.com>
2019-11-08 06:20:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -92,6 +92,7 @@ def apply_coupons(cart, coupons)
     #end
       
     if cart_item  === find_coupon_item && cart_count = coupon_num
+      
       # updated_cart.push({ of the following new info})
         # change name :item = "#{coupon_item} W/COUPON"
         # change price :price = coupon :cost/ coupon :num .round(3)
